4cef0d5dc00500f67c8c80a1476ef2a266ee0586
gbTechh <8kdots@gmail.com>
2022-09-16 10:17:32 -0500

    [FIX] FIx Import component Layout on  page 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,12 @@
 import * as React from "react"
-import { LayoutPrincipal } from "../components"
+import { LayoutHome } from "../components"
 
 
 
 export default function NotFound() {
   return (
-    <LayoutPrincipal title="404: Page not found">
+    <LayoutHome title="404: Page not found">
 
-    </LayoutPrincipal>
+    </LayoutHome>
   )
 }
