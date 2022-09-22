2da74783472ef1120418cc3b5492e5cce580c543
gbTechh <8kdots@gmail.com>
2022-09-21 23:17:00 -0500

    [END] Finish Chef Section

---
+++ src/hooks/useGetDataChef.js
@@ -0,0 +1,40 @@
+import { graphql, useStaticQuery } from 'gatsby'
+
+export const useGetDataChef = () => {
+
+  const data = useStaticQuery(graphql`
+  {
+    allWpSection{
+      edges{
+        node{
+          slug
+          chef{
+            titles {
+              mainTitle
+              subtitle
+            }
+            sign{
+              gatsbyImage(width:250)
+            }
+            description
+            nameChefs
+            jobPosition
+            imageChefs{
+              gatsbyImage(width:600, height:800)
+            }
+          }
+        }
+      }
+    }	
+  }  
+  `
+  );
+
+
+  const dataSource = data.allWpSection.edges.filter(e => e.node.slug === 'chef')[0].node;
+
+
+  return dataSource;
+
+}
+
